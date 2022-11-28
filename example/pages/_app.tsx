f5ed5e3e89827fad18412b18c6ca2b0def811f22
Mohammad Shahin <59062739+MohammadShahin@users.noreply.github.com>
2022-11-28 20:40:53 +0300

    merge fix

--- example/pages/_app.tsx
+++ example/pages/_app.tsx
@@ -1,19 +1,27 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import { getDefaultProvider } from 'ethers';
 import type { AppProps } from 'next/app';
-import { createClient, WagmiConfig, chain } from 'wagmi';
-import { ZeroWalletConnector } from 'zero-wallet-wagmi-connector';
-import { ChakraProvider } from '@chakra-ui/react'
-import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { chain,createClient, WagmiConfig } from 'wagmi';
+import {
+    RecoveryConfig,
+    ZeroWalletConnector,
+    ZeroWalletConnectorOptions} from 'zero-wallet-wagmi-connector';
+// import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
 
-const zeroWalletConnectorOptions = {
-    jsonRpcProviderUrls:{
-        5: `https://eth-goerli.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`,
+const googleRecoveryOption: RecoveryConfig = {
+    type: 'google-web-recovery',
+    googleClientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
+    allowMultiKeys: false,
+    handleExistingKey: 'Overwrite'
+};
+
+const zeroWalletConnectorOptions: ZeroWalletConnectorOptions = {
+    jsonRpcProviderUrls:{ 5: `https://eth-goerli.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`,
         10: undefined,
         137: undefined,
         42220: undefined,
     },
     store: 'browser',
-    recoveryMechanism: 'google',
     zeroWalletServerDomain: process.env.NEXT_PUBLIC_BACKEND_DOMAIN!,
     gasTankName: 'testGasTankName'
 }
@@ -24,7 +32,7 @@ const connector = new ZeroWalletConnector({
 });
 
 
-const provider = getDefaultProvider(chain.goerli.id)
+const provider = getDefaultProvider(chain.goerli.id);
 
 const client = createClient({
     autoConnect: false,
@@ -37,7 +45,7 @@ const client = createClient({
         //     },
         // }),
         connector
-    ],
+    ]
 });
 
 export default function App({ Component, pageProps }: AppProps) {
